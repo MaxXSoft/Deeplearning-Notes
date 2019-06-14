b6b6222854e86190aa4e27ad3437d0fcf1540479
MaxXSoft <x@MaxXSoft.net>
2019-06-14 14:58:08 +0800

    (conv, pool) fixed some bugs

--- blog-record/record-4/cnn-py/conv.py
+++ blog-record/record-4/cnn-py/conv.py
@@ -2,97 +2,6 @@ import numpy as np
 from layer import Layer
 
 
-def __conv_pad(mat_nhwc, pad):
-  dim = ((0, 0), (pad, pad), (pad, pad), (0, 0))
-  return np.pad(mat_nhwc, dim, 'constant', constant_values=0)
-
-
-def __conv_step(slice_ffc, kernel_ffc, bias):
-  ans = np.multiply(slice_ffc, kernel_ffc) + bias
-  return np.sum(ans)
-
-
-def __conv(mat_nhwc, kernel_ffck, bias_111k, **kwargs):
-  # extract shapes
-  n, h, w, c_m = mat_nhwc.shape
-  f, f, c_k, kc = kernel_ffck.shape
-  assert c_m == c_k
-  # extract params
-  stride = kwargs.get('stride', 1)
-  pad = kwargs.get('pad', 0)
-  # compute output shape
-  oh = int((h - f + 2 * pad) / stride) + 1
-  ow = int((w - f + 2 * pad) / stride) + 1
-  # initialize output
-  z = np.zeros((n, oh, ow, kc))
-  mat_pads = __conv_pad(mat_nhwc, pad)
-  # compute
-  for i in range(n):
-    mat_pad = mat_pads[i]
-    for y in range(oh):
-      for x in range(ow):
-        for k in range(kc):
-          # find the corners of the current slice
-          v_start = y * stride
-          v_end = v_start + f
-          h_start = x * stride
-          h_end = h_start + f
-          # get slice
-          slice_ffc = mat_pad[v_start:v_end, h_start:h_end, :]
-          # get output
-          cur_kernel = kernel_ffck[..., k]
-          cur_bias = bias_111k[..., k]
-          z[i, y, x, k] = __conv_step(
-              slice_ffc, cur_kernel, cur_bias)
-  assert z.shape == (n, oh, ow, kc)
-  # save in cache
-  cache = (mat_nhwc, kernel_ffck, bias_111k, kwargs)
-  return z, cache
-
-
-def __conv_back(dz, cache):
-  # extract cache
-  mat, kernel, _, args = cache
-  # extract shapes of mat
-  n, h, w, c = mat.shape
-  # extract shapes of kernel
-  f, f, c, kc = kernel.shape
-  # extract params
-  stride = args.get('stride', 1)
-  pad = args.get('pad', 0)
-  # extract shapes of dz
-  n, oh, ow, kc = dz.shape
-  # initialize output
-  dmat = np.zeros((n, h, w, c))
-  dk = np.zeros((f, f, c, kc))
-  db = np.zeros((1, 1, 1, kc))
-  # pad mat and dmat
-  mat_pads = __conv_pad(mat, pad)
-  dm_pads = __conv_pad(dmat, pad)
-  # compute
-  for i in range(n):
-    mat_pad = mat_pads[i]
-    dm_pad = dm_pads[i]
-    for y in range(oh):
-      for x in range(ow):
-        for k in range(kc):
-          # find the corners of the current slice
-          v_start = y * stride
-          v_end = v_start + f
-          h_start = x * stride
-          h_end = h_start + f
-          # get slice
-          mat_slice = mat_pad[v_start:v_end, h_start:h_end, :]
-          # update gradients
-          cur_dz = dz[i, y, x, k]
-          dm_pad[v_start:v_end, h_start:h_end, :] += kernel[..., k] * cur_dz
-          dk[..., c] += mat_slice * cur_dz
-          db[..., c] += cur_dz
-    dmat[i, ...] = dm_pad[pad:-pad, pad:-pad, :]
-  assert dmat.shape == (n, h, w, c)
-  return dmat, dk, db
-
-
 # convolutional layer
 class ConvLayer(Layer):
   def __init__(self, ih, iw, ic, f, oc, **kwargs):
@@ -126,13 +35,104 @@ class ConvLayer(Layer):
     self.__dk = None
     self.__db = None
 
+  @staticmethod
+  def __conv_pad(mat_nhwc, pad):
+    dim = ((0, 0), (pad, pad), (pad, pad), (0, 0))
+    return np.pad(mat_nhwc, dim, 'constant', constant_values=0)
+
+  @staticmethod
+  def __conv_step(slice_ffc, kernel_ffc, bias):
+    ans = np.multiply(slice_ffc, kernel_ffc) + bias
+    return np.sum(ans)
+
+  @staticmethod
+  def __conv(mat_nhwc, kernel_ffck, bias_111k, **kwargs):
+    # extract shapes
+    n, h, w, c_m = mat_nhwc.shape
+    f, f, c_k, kc = kernel_ffck.shape
+    assert c_m == c_k
+    # extract params
+    stride = kwargs.get('stride', 1)
+    pad = kwargs.get('pad', 0)
+    # compute output shape
+    oh = int((h - f + 2 * pad) / stride) + 1
+    ow = int((w - f + 2 * pad) / stride) + 1
+    # initialize output
+    z = np.zeros((n, oh, ow, kc))
+    mat_pads = ConvLayer.__conv_pad(mat_nhwc, pad)
+    # compute
+    for i in range(n):
+      mat_pad = mat_pads[i]
+      for y in range(oh):
+        for x in range(ow):
+          for k in range(kc):
+            # find the corners of the current slice
+            v_start = y * stride
+            v_end = v_start + f
+            h_start = x * stride
+            h_end = h_start + f
+            # get slice
+            slice_ffc = mat_pad[v_start:v_end, h_start:h_end, :]
+            # get output
+            cur_kernel = kernel_ffck[..., k]
+            cur_bias = bias_111k[..., k]
+            z[i, y, x, k] = ConvLayer.__conv_step(
+                slice_ffc, cur_kernel, cur_bias)
+    assert z.shape == (n, oh, ow, kc)
+    # save in cache
+    cache = (mat_nhwc, kernel_ffck, bias_111k, kwargs)
+    return z, cache
+
+  @staticmethod
+  def __conv_back(dz, cache):
+    # extract cache
+    mat, kernel, _, args = cache
+    # extract shapes of mat
+    n, h, w, c = mat.shape
+    # extract shapes of kernel
+    f, f, c, kc = kernel.shape
+    # extract params
+    stride = args.get('stride', 1)
+    pad = args.get('pad', 0)
+    # extract shapes of dz
+    n, oh, ow, kc = dz.shape
+    # initialize output
+    dmat = np.zeros((n, h, w, c))
+    dk = np.zeros((f, f, c, kc))
+    db = np.zeros((1, 1, 1, kc))
+    # pad mat and dmat
+    mat_pads = ConvLayer.__conv_pad(mat, pad)
+    dm_pads = ConvLayer.__conv_pad(dmat, pad)
+    # compute
+    for i in range(n):
+      mat_pad = mat_pads[i]
+      dm_pad = dm_pads[i]
+      for y in range(oh):
+        for x in range(ow):
+          for k in range(kc):
+            # find the corners of the current slice
+            v_start = y * stride
+            v_end = v_start + f
+            h_start = x * stride
+            h_end = h_start + f
+            # get slice
+            mat_slice = mat_pad[v_start:v_end, h_start:h_end, :]
+            # update gradients
+            cur_dz = dz[i, y, x, k]
+            dm_pad[v_start:v_end, h_start:h_end, :] += kernel[..., k] * cur_dz
+            dk[..., c] += mat_slice * cur_dz
+            db[..., c] += cur_dz
+      dmat[i, ...] = dm_pad[pad:-pad, pad:-pad, :]
+    assert dmat.shape == (n, h, w, c)
+    return dmat, dk, db
+
   def forward(self, x):
-    z, cache = __conv(x, self.__kernel, self.__bias, **self.__params)
+    z, cache = self.__conv(x, self.__kernel, self.__bias, **self.__params)
     self.__cache = cache
     return z
 
   def backprop(self, d):
-    dmat, dk, db = __conv_back(d, self.__cache)
+    dmat, dk, db = self.__conv_back(d, self.__cache)
     self.__dk = dk
     self.__db = db
     return dmat
@@ -141,19 +141,3 @@ class ConvLayer(Layer):
     self.__kernel -= alpha * self.__dk
     self.__bias -= alpha * self.__db
     pass
-
-
-if __name__ == '__main__':
-  np.random.seed(1)
-  mat = np.random.randn(10, 4, 4, 3)
-  kernel = np.random.randn(2, 2, 3, 8)
-  bias = np.random.randn(1, 1, 1, 8)
-  args = {"pad": 2, "stride": 1}
-  Z, cache_conv = __conv(mat, kernel, bias, **args)
-  print("Z's mean =", np.mean(Z))
-  print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
-  np.random.seed(1)
-  dA, dW, db = __conv_back(Z, cache_conv)
-  print("dA_mean =", np.mean(dA))
-  print("dW_mean =", np.mean(dW))
-  print("db_mean =", np.mean(db))
--- blog-record/record-4/cnn-py/pool.py
+++ blog-record/record-4/cnn-py/pool.py
@@ -2,89 +2,6 @@ import numpy as np
 from layer import Layer
 
 
-def __pool(mat_nhwc, **kwargs):
-  # extract shapes
-  n, h, w, c = mat_nhwc.shape
-  # extract params
-  f = kwargs.get('f', 2)
-  stride = kwargs.get('stride', 2)
-  mode = kwargs.get('mode', 'max')
-  # compute output shape
-  oh = int((h - f) / stride) + 1
-  ow = int((w - f) / stride) + 1
-  # initialize output
-  z = np.zeros((n, oh, ow, c))
-  # compute
-  for i in range(n):
-    for y in range(oh):
-      for x in range(ow):
-        for k in range(c):
-          # find the corners of the current slice
-          v_start = y * stride
-          v_end = v_start + f
-          h_start = x * stride
-          h_end = h_start + f
-          # get slice
-          slice_nhwc = mat_nhwc[i, v_start:v_end, h_start:h_end, k]
-          # get output
-          if mode == 'max':
-            z[i, y, x, k] = np.max(slice_nhwc)
-          elif mode == 'average':
-            z[i, y, x, k] = np.mean(slice_nhwc)
-  assert z.shape == (n, oh, ow, c)
-  # store in cache
-  cache = (mat_nhwc, kwargs)
-  return z, cache
-
-
-def __get_pool_mask(x):
-  return x == np.max(x)
-
-
-def __distribute_value(dz, shape):
-  h, w = shape
-  average = dz / (h * w)
-  return np.ones(shape) * average
-
-
-def __pool_back(dz, cache, mode=None):
-  # extract cache
-  mat, args = cache
-  # extract params
-  f = args.get('f', 2)
-  stride = args.get('stride', 2)
-  if not mode:
-    mode = args.get('mode', 'max')
-  # extract shapes
-  n, _, _, _ = mat.shape
-  n, oh, ow, c = dz.shape
-  # initialize dmat
-  dmat = np.zeros(mat.shape)
-  # compute
-  for i in range(n):
-    cur_mat = mat[i]
-    for y in range(oh):
-      for x in range(ow):
-        for k in range(c):
-          # find the corners of the current slice
-          v_start = y * stride
-          v_end = v_start + f
-          h_start = x * stride
-          h_end = h_start + f
-          # compute output
-          cur_dz = dz[i, y, x, k]
-          if mode == 'max':
-            cur_slice = cur_mat[v_start:v_end, h_start:h_end, k]
-            mask = __get_pool_mask(cur_slice)
-            dmat[i, v_start:v_end, h_start:h_end,
-                 k] += np.multiply(mask, cur_dz)
-          elif mode == 'average':
-            dmat[i, v_start:v_end, h_start:h_end,
-                 k] += __distribute_value(cur_dz, (f, f))
-  assert dmat.shape == mat.shape
-  return dmat
-
-
 # pooling layer
 class PoolLayer(Layer):
   def __init__(self, **kwargs):
@@ -105,31 +22,96 @@ class PoolLayer(Layer):
     self.__params = kwargs
     self.__cache = None
 
+  @staticmethod
+  def __pool(mat_nhwc, **kwargs):
+    # extract shapes
+    n, h, w, c = mat_nhwc.shape
+    # extract params
+    f = kwargs.get('f', 2)
+    stride = kwargs.get('stride', 2)
+    mode = kwargs.get('mode', 'max')
+    # compute output shape
+    oh = int((h - f) / stride) + 1
+    ow = int((w - f) / stride) + 1
+    # initialize output
+    z = np.zeros((n, oh, ow, c))
+    # compute
+    for i in range(n):
+      for y in range(oh):
+        for x in range(ow):
+          for k in range(c):
+            # find the corners of the current slice
+            v_start = y * stride
+            v_end = v_start + f
+            h_start = x * stride
+            h_end = h_start + f
+            # get slice
+            slice_nhwc = mat_nhwc[i, v_start:v_end, h_start:h_end, k]
+            # get output
+            if mode == 'max':
+              z[i, y, x, k] = np.max(slice_nhwc)
+            elif mode == 'average':
+              z[i, y, x, k] = np.mean(slice_nhwc)
+    assert z.shape == (n, oh, ow, c)
+    # store in cache
+    cache = (mat_nhwc, kwargs)
+    return z, cache
+
+  @staticmethod
+  def __get_pool_mask(x):
+    return x == np.max(x)
+
+  @staticmethod
+  def __distribute_value(dz, shape):
+    h, w = shape
+    average = dz / (h * w)
+    return np.ones(shape) * average
+
+  @staticmethod
+  def __pool_back(dz, cache, mode=None):
+    # extract cache
+    mat, args = cache
+    # extract params
+    f = args.get('f', 2)
+    stride = args.get('stride', 2)
+    if not mode:
+      mode = args.get('mode', 'max')
+    # extract shapes
+    n, _, _, _ = mat.shape
+    n, oh, ow, c = dz.shape
+    # initialize dmat
+    dmat = np.zeros(mat.shape)
+    # compute
+    for i in range(n):
+      cur_mat = mat[i]
+      for y in range(oh):
+        for x in range(ow):
+          for k in range(c):
+            # find the corners of the current slice
+            v_start = y * stride
+            v_end = v_start + f
+            h_start = x * stride
+            h_end = h_start + f
+            # compute output
+            cur_dz = dz[i, y, x, k]
+            if mode == 'max':
+              cur_slice = cur_mat[v_start:v_end, h_start:h_end, k]
+              mask = PoolLayer.__get_pool_mask(cur_slice)
+              dmat[i, v_start:v_end, h_start:h_end,
+                   k] += np.multiply(mask, cur_dz)
+            elif mode == 'average':
+              dmat[i, v_start:v_end, h_start:h_end,
+                   k] += PoolLayer.__distribute_value(cur_dz, (f, f))
+    assert dmat.shape == mat.shape
+    return dmat
+
   def forward(self, x):
-    z, cache = __pool(x, **self.__params)
+    z, cache = self.__pool(x, **self.__params)
     self.__cache = cache
     return z
 
   def backprop(self, d):
-    return __pool_back(d, self.__cache)
+    return self.__pool_back(d, self.__cache)
 
   def gradient(self, alpha):
     pass
-
-
-if __name__ == '__main__':
-  np.random.seed(1)
-  A_prev = np.random.randn(5, 5, 3, 2)
-  args = {"stride": 1, "f": 2}
-  A, cache = __pool(A_prev, **args)
-  dA = np.random.randn(5, 4, 2, 2)
-
-  dA_prev = __pool_back(dA, cache, mode="max")
-  print("mode = max")
-  print('mean of dA = ', np.mean(dA))
-  print('dA_prev[1,1] = ', dA_prev[1, 1])
-  print()
-  dA_prev = __pool_back(dA, cache, mode="average")
-  print("mode = average")
-  print('mean of dA = ', np.mean(dA))
-  print('dA_prev[1,1] = ', dA_prev[1, 1])
